19277345292465dea7792198f58fd557f8dd1aef
denisvieir <denisvieira07@gmail.com>
2016-12-25 18:24:38 -0200

    Mudanças no AllowedHosts2

--- eventex/settings.py
+++ eventex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 #ALLOWED_HOSTS = ['127.0.0.1']
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='eventex-denisvieira.herokuapp.com')
 
 
 # Application definition
