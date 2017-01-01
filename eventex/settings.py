d033142a5d67515233d589ccdca3305d62d7a1c0
denis_vieira <denisvieira07@gmail.com>
2017-01-01 21:35:11 -0200

    Mudanças no Settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-#ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='eventex-denisvieira.herokuapp.com')
+ALLOWED_HOSTS = ['eventex-denisvieira.herokuapp.com']
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='eventex-denisvieira.herokuapp.com')
 
 
 # Application definition
